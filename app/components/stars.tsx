f3417bf2aa6dd79e6d516471995e6ec0dcd07160
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-10-27 12:16:19 +0530

    added stars animation

---
+++ app/components/stars.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Stars = () => {
+  return (
+    <section>
+        {/* <h2 className=''>stars</h2> */}
+        <div className='star star1'></div>
+        <div className='star star2'></div>
+        <div className='star star3'></div>
+        <div className='star star4'></div>
+        <div className='star star5'></div>
+        <div className='star star6'></div>
+        <div className='star star7'></div>
+        <div className='star star8'></div>
+    </section>
+  )
+}
+
+export default Stars
